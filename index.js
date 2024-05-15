38be900bf6645218b73fbfd82817789d3ec76e92
phantomgithub <011235813phantom@gmail.com>
2024-05-15 21:40:06 +0530

    added a index.js file to generete the board as well as a simple css for testing purposes

---
+++ index.js
@@ -0,0 +1,55 @@
+class Square
+    {
+        constructor(color,index_id,piece)
+        {
+            this.color=color;
+            this.index_id=index_id;
+            this.piece=piece;
+        }
+         set_piece(piece)
+        {
+            this.piece=piece;
+
+        }
+        render()
+        {
+            let div=document.createElement('div');
+            div.setAttribute("class","square");
+            div.setAttribute("id",this.index_id);
+            div.style.backgroundColor=this.color;
+
+            if(this.piece)
+            {
+                let pieceImg=document.createElement('img');
+                pieceImg.setAttribute('src',this.piece);
+                div.appendChild(pieceImg);
+            }
+        return div;
+        }
+    }
+
+    class Board
+        {
+            constructor()
+            {
+                this.squares=[];
+            }
+            render()
+            {
+                const board=document.createElement('div');
+                board.id="board";
+                this.squares.forEach(squares=>{
+                    squares.render();
+                    board.appendChild(squares);
+                }
+
+                )
+                return board;
+            }
+        }
+        const board = new Board();
+ 
+board.squares.push(new Square('yellow', 'a1', 'path_to_piece_image'));
+
+const boardElement = board.render();
+document.body.appendChild(boardElement);
